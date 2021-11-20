0d62285a13111c58404d0b7acfff5d64b92de5f6
Andrew Dansby <andydansby@gmail.com>
2021-11-20 13:45:11 -0500

    Add files via upload

---
+++ RAM3/BANK3.asm
@@ -0,0 +1,7 @@
+;org 49152
+;defb 33
+
+SECTION BANK_03
+    PUBLIC BANK_03_Variable
+    BANK_03_Variable:
+    defb 33
---
+++ RAM3/error.bat
@@ -0,0 +1,6 @@
+echo on
+rem AN ERROR HAS OCCURED in RAM3
+rem press CTRL-C to exit
+
+rem exit /b
+pause
